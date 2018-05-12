7309a471a35b68d10b6008e61d5c3c4ba969f3d1
itay2805 <itay2805@protonmail.com>
2018-05-12 15:25:15 +0300

    Added Window API
    the window API is now fully implemented!

--- TomatOS/src/os/os.cpp
+++ TomatOS/src/os/os.cpp
@@ -4,6 +4,7 @@
 #include <apis/Term.hpp>
 #include <apis/OS.hpp>
 #include <apis/Colors.h>
+#include <apis/Window.hpp>
 
 
 extern "C" void startup() {
@@ -14,7 +15,14 @@ extern "C" void startup() {
 	Term::SetTextColor(Colors::WHITE);
 	Term::Clear();
 
+	Window window(Term::Current(), 10, 10, 10, 10, true);
+	ITerm* old = Term::Redirect((ITerm*)&window);
+
+	Term::SetBackgroundColor(Colors::RED);
+	Term::Clear();
+
 	const char* version = OS::Version();
 	Term::Write(version);
 
+	Term::Redirect(old);
 }
