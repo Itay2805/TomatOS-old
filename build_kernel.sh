9784838c81d387e308cc26e947dc24c3c60c3f70
itay2805 <itay2805@protonmail.com>
2018-10-02 19:10:08 +0300

    Improved building speed by forking the iterations over file, making so all the compiling is done in parallel.

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,113 +8,120 @@ echo "Preparing kernel compilation"
 should_link_kernel="false"
 mkdir "build" 2> /dev/null
 
-# Compiling Kernel
-echo "Compiling TomatKernel"
-for d in $(find TomatKernel -type d 2> /dev/null)
-do
+process_file_c() {
+	f=$1
+
+	of="$f.o"
+	of="build/$of"
+	
+	if [ -e "build/$f.modtime" ]; then
+		if [ "$(stat -c %y $f)" == "$(cat build/$f.modtime)" ]; then
+			echo "	$f > not changed, skipping"
+		else
+			echo "  $f > $of"
+			gcc $CCFLAGS -c "./$f" -o "./$of" 
+			echo "$(stat -c %y $f)" >> "build/$f.modtime"
+		fi
+	else
+		echo "  $f > $of"
+		gcc $CCFLAGS -c "./$f" -o "./$of"
+		echo "$(stat -c %y $f)" >> "build/$f.modtime"
+	fi
+}
+
+process_file_cpp() {
+	f=$1
+
+	of="$f.o"
+	of="build/$of"
+
+	if [ -e "build/$f.modtime" ]; then
+		if [ "$(stat -c %y $f)" == "$(cat build/$f.modtime)" ]; then
+			echo "	$f > not changed, skipping"
+		else
+			echo "  $f > $of"
+			g++ $GCCPARAMS -c "./$f" -o "./$of" 
+			echo "$(stat -c %y $f)" >> "build/$f.modtime"
+		fi
+	else
+		echo "  $f > $of"
+		g++ $GCCPARAMS -c "./$f" -o "./$of" 
+		echo "$(stat -c %y $f)" >> "build/$f.modtime"
+	fi
+}
+
+process_file_asm() {
+	f=$1
+
+	of="$f.o"
+	of="build/$of"
+
+	if [ -e "build/$f.modtime" ]; then
+		if [ "$(stat -c %y $f)" == "$(cat build/$f.modtime)" ]; then
+			echo "	$f > not changed, skipping"
+		else
+			echo "  $f > $of"
+			nasm $NASMPARAMS "./$f" -o "./$of"
+			echo "$(stat -c %y $f)" >> "build/$f.modtime"
+		fi
+	else
+		echo "  $f > $of"
+		nasm $NASMPARAMS "./$f" -o "./$of"
+		echo "$(stat -c %y $f)" >> "build/$f.modtime"
+	fi
+}
+
+process_folder() {
+	d=$1
+
 	mkdir "build/$d" 2> /dev/null
 
 	# Build C files
 	for f in $(find $d/*.c 2> /dev/null)
 	do
-		of="$f.o"
-		of="build/$of"
-		objects="$objects $of"
-		
-		if [ -e "build/$f.modtime" ]; then
-			if [ "$(stat -c %y $f)" == "$(cat build/$f.modtime)" ]; then
-				echo "	$f > not changed, skipping"
-			else
-				echo "  $f > $of"
-				gcc $CCFLAGS -c "./$f" -o "./$of" 
-				echo "$(stat -c %y $f)" >> "build/$f.modtime"
-				should_link_kernel="true"
-			fi
-		else
-			echo "  $f > $of"
-			gcc $CCFLAGS -c "./$f" -o "./$of"
-			echo "$(stat -c %y $f)" >> "build/$f.modtime"
-			should_link_kernel="true"
-		fi
+		process_file_c $f &
 	done
 
 	# Build cpp files
 	for f in $(find $d/*.cpp 2> /dev/null)
 	do
-		of="$f.o"
-		of="build/$of"
-		objects="$objects $of"
-
-		if [ -e "build/$f.modtime" ]; then
-			if [ "$(stat -c %y $f)" == "$(cat build/$f.modtime)" ]; then
-				echo "	$f > not changed, skipping"
-			else
-				echo "  $f > $of"
-				g++ $GCCPARAMS -c "./$f" -o "./$of" 
-				echo "$(stat -c %y $f)" >> "build/$f.modtime"
-				should_link_kernel="true"
-			fi
-		else
-			echo "  $f > $of"
-			g++ $GCCPARAMS -c "./$f" -o "./$of" 
-			echo "$(stat -c %y $f)" >> "build/$f.modtime"
-			should_link_kernel="true"
-		fi
+		process_file_cpp $f &
 	done
 
 	# Build asm files
 	for f in $(find $d/*.asm 2> /dev/null)
 	do
-		of="$f.o"
-		of="build/$of"
-		objects="$objects $of"
-
-		if [ -e "build/$f.modtime" ]; then
-			if [ "$(stat -c %y $f)" == "$(cat build/$f.modtime)" ]; then
-				echo "	$f > not changed, skipping"
-			else
-				echo "  $f > $of"
-				nasm $NASMPARAMS "./$f" -o "./$of"
-				echo "$(stat -c %y $f)" >> "build/$f.modtime"
-				should_link_kernel="true"
-			fi
-		else
-			echo "  $f > $of"
-			nasm $NASMPARAMS "./$f" -o "./$of"
-			echo "$(stat -c %y $f)" >> "build/$f.modtime"
-			should_link_kernel="true"
-		fi
+		process_file_asm $f &		
 	done
+
+	# Wait for file processing
+	for job in `jobs -p`
+	do
+		wait $job
+	done
+}
+
+# Compiling Kernel
+echo "Compiling TomatKernel"
+for d in $(find TomatKernel -type d 2> /dev/null)
+do
+	process_folder $d & 
 done
 
-# Compile 
+# Wait for folder processing
+for job in `jobs -p`
+do
+	wait $job
+done
+
+objects="$(find ./build -type f -name \*.o)"
 
 # Linking
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-
-	echo $should_link_kernel
-
-	if [ "$should_link_kernel" == "true" ]; then
-		echo "Linking Kernel"
-		ld $LDPARAMS -T TomatKernel/linker.ld -o kernel.debug.elf $objects
-		echo "Removing debug symbols (for non debug version)"
-		objcopy --strip-debug kernel.debug.elf kernel.elf
-	else
-		if [ -e "kernel.debug.elf" ]; then
-			echo "Nothing changed, skipping kernel-link"
-		else
-			echo "No kernel.debug.elf file found, linking kernel"
-			ld $LDPARAMS -T TomatKernel/linker.ld -o kernel.debug.elf $objects
-		fi
-
-		if [ -e "kernel.elf" ]; then
-			echo "Non-Debug version already exists"
-		else
-			echo "Removing debug symbols (for non debug version)"
-			objcopy --strip-debug kernel.debug.elf kernel.elf
-		fi
-	fi
-
+	echo "Linking Kernel"
+	ld $LDPARAMS -T TomatKernel/linker.ld -o kernel.debug.elf $objects
+	echo "Removing debug symbols (for non debug version)"
+	objcopy --strip-debug kernel.debug.elf kernel.elf
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
 	echo "Skipping link (can't link on windows)"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
