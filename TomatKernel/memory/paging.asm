bbcd0742528eadf1cf48b4f81fd3484510947b39
itay2805 <itay2805@protonmail.com>
2018-12-08 17:12:07 +0200

    Added stack frames Will be useful for the debugger

--- TomatKernel/memory/paging.asm
+++ TomatKernel/memory/paging.asm
@@ -3,19 +3,37 @@
 [global paging_is_enabled]
 
 paging_enable:
+	push ebp
+	mov ebp, esp
+
 	mov eax, cr0
     or eax, 0x80000000
     mov cr0, eax
+	
+	mov esp, ebp
+	pop ebp
 	ret
 
 paging_disable:
+	push ebp
+	mov ebp, esp
+
 	mov eax, cr0 
     and eax, 0xFFFFFFFF7FFFFFFF
     mov cr0, eax
+
+	mov esp,ebp
+	pop ebp
 	ret
 
 paging_is_enabled:
+	push ebp
+	mov ebp, esp
+
 	mov eax, cr0
 	and eax, 0x80000000
 	shr eax, 30
+
+	mov esp, ebp
+	pop ebp
 	ret
